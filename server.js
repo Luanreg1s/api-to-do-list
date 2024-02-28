3e78b0e635289d1e6206c959b9dc7e30b5d62f09
Luanreg1s <Luanregispereira060@gmail.com>
2024-02-28 19:58:33 -0300

    add cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const prisma = require('./config/db');
-var bodyParser = require('body-parser')
+const cors = require("cors")
 
 dotenv.config();
 const app = express();
 const porta = 3000;
+app.use(cors({
+    origin:"*"
+}))
 
 require('./routes/todoList')(app);
 require('./routes/autenticacao.js')(app);
